2a5fae07b8753a08f6d28fcc368ff16aed863ed2
celsoaf <celsoaf@gmail.com>
2013-11-24 00:11:07 +0000

    Add TwitterSpyService

---
+++ 02 - Codigo/BrandAnalytics/TwitterSpy/ITwitterSpyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterSpy
+{
+    [ServiceContract]
+    public interface ITwitterSpyService
+    {
+        [OperationContract]
+        int SpyTopics(string topic, DateTime start, DateTime end);
+    }
+}
