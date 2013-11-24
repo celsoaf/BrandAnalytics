24abb1bd30a21e87a9ba6bc14d2d9cbf7009b86d
celsoaf <celsoaf@gmail.com>
2013-11-24 00:59:11 +0000

    add emailService interface

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics/Services/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BrandAnalytics.Services
+{
+    public class EmailService
+    {
+        public void Send(string to, string subject, string body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
