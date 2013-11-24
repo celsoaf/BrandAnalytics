e1a8c23d025f312f2fde114600803a14359f80d9
celsoaf <celsoaf@gmail.com>
2013-11-24 00:12:10 +0000

    Add Service contract

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics/IBrandAnalyticsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandAnalytics
+{
+    [ServiceContract]
+    public interface IBrandAnalyticsService
+    {
+        [OperationContract]
+        int RequestStudy(string mark);
+
+        [OperationContract]
+        string GetState(int token);
+    }
+}
