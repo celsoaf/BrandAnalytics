efb4f43fb1fc241615ebac4da7fac660f7776f0b
celsoaf <celsoaf@gmail.com>
2013-11-24 15:53:23 +0000

    add client contract

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics/Activities/SaveNewStudyActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrandAnalytics.Data.Models;
+
+namespace BrandAnalytics.Activities
+{
+    public class SaveNewStudyActivity : CodeActivity
+    {
+        public InArgument<string> UserName { get; set; }
+        public InOutArgument<string> Mark { get; set; }
+        public OutArgument<int> ActivityId { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            using (var ctx = new BrandAnalyticsDB())
+            {
+                var userName = UserName.Get(context);
+                var mark = Mark.Get(context);
+
+                var client = ctx.Clients.FirstOrDefault(c => c.UserName == userName);
+                if (client != null)
+                {
+                    var study = new Study()
+                    {
+                        Mark=mark
+                    };
+
+                    client.Studies.Add(study);
+
+                    ctx.SaveChanges();
+
+                    ActivityId.Set(context, study.Id);
+                }
+            }
+        }
+    }
+}
