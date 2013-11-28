b096fdd55b90a2e69e6332ac5da5c5457efe95d8
celsoaf <celsoaf@gmail.com>
2013-11-28 22:38:19 +0000

    Add notification and StudyEmployee controllers

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics.Web/Controllers/NotificationController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandAnalytics.Data.Models;
+
+namespace BrandAnalytics.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+        //
+        // GET: /Notification/
+
+        public ActionResult Index()
+        {
+            var userName = User.Identity.Name;
+            using (var ctx = new BrandAnalyticsDB())
+            {
+                var user = ctx.Clients.FirstOrDefault(c => c.UserName == userName);
+                if (user != null)
+                {
+                    var list = user.Notifications.ToList();
+
+                    return View(list);
+                }
+            }
+
+            return View();
+        }
+
+        //
+        // GET: /Study/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            using (var ctx = new BrandAnalyticsDB())
+            {
+                var model = ctx.Notifications
+                    .Where(s => s.Id == id)
+                    .FirstOrDefault();
+
+                return View(model);
+            }
+        }
+
+        //
+        // POST: /Study/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (var ctx = new BrandAnalyticsDB())
+                {
+                    var model = ctx.Notifications
+                        .Where(s => s.Id == id)
+                        .FirstOrDefault();
+
+                    ctx.Notifications.Remove(model);
+
+                    ctx.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics.Web/Controllers/StudyEmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandAnalytics.Data.Models;
+using BrandAnalytics.Web.Models;
+using BrandAnalytics.Web.Utils;
+
+namespace BrandAnalytics.Web.Controllers
+{
+    public class StudyEmployeeController : Controller
+    {
+        //
+        // GET: /Study/
+
+        public ActionResult Index()
+        {
+            using (var ctx = new BrandAnalyticsDB())
+            {
+                var list = ctx.Studies.Select(StudyUtils.TranslateStudie).ToList();
+
+                return View(list);
+            }
+        }
+
+    }
+}
