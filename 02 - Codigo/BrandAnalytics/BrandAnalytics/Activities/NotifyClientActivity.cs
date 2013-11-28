757f5c998b2728ccd4536cd71ee2289e8b8deacf
celsoaf <celsoaf@gmail.com>
2013-11-28 21:56:57 +0000

    Separate notifies

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics/Activities/NotifyClientActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BrandAnalytics.Data.Enums;
+using BrandAnalytics.Data.Models;
+using BrandAnalytics.Infrastructure.Services;
+
+namespace BrandAnalytics.Activities
+{
+    public class NotifyClientActivity : CodeActivity
+    {
+        public InArgument<int> StudyId { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            using (var ctx = new BrandAnalyticsDB())
+            {
+                var id = StudyId.Get(context);
+
+                var study = ctx.Studies.FirstOrDefault(s => s.Id == id);
+                if (study != null)
+                {
+                    study.Client.Notifications.Add(new Notification()
+                    {
+                        Subject = string.Format("Study {0} in state {1}", study.Mark, study.State),
+                        Body = string.Format("Study {0} in state {1}", study.Mark, study.State)
+                    });
+                    //emailService.Send(study.Client.Email,
+                    //            string.Format("Study {0} in state {1}", study.Mark, study.State),
+                    //            string.Format("Body"));
+
+
+                    ctx.SaveChanges();
+                }
+            }
+        }
+    }
+}
