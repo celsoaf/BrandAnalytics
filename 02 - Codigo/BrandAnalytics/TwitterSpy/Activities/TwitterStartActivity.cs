541e5f91958f65d67c806b8415e61fe2f7b22329
celsoaf <celsoaf@gmail.com>
2013-11-28 00:49:38 +0000

    add twitterSpy activities

---
+++ 02 - Codigo/BrandAnalytics/TwitterSpy/Activities/TwitterStartActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TwitterSpy.Services;
+
+namespace TwitterSpy.Activities
+{
+    public class TwitterStartActivity : CodeActivity
+    {
+        public InArgument<string[]> Topics { get; set; }
+        public OutArgument<ITwitterService[]> Instances { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var topics = Topics.Get(context);
+
+            var list = new List<ITwitterService>();
+
+            foreach (var item in topics)
+            {
+                var service = new TwitterService();
+
+                service.StartStreaming(item);
+
+                list.Add(service);
+            }
+
+            Instances.Set(context, list.ToArray());
+        }
+    }
+}
---
+++ 02 - Codigo/BrandAnalytics/TwitterSpy/Activities/TwitterStopActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TwitterSpy.Models;
+using TwitterSpy.Services;
+
+namespace TwitterSpy.Activities
+{
+    public class TwitterStopActivity : CodeActivity
+    {
+        public InArgument<ITwitterService[]> Instances { get; set; }
+        public OutArgument<ReportModel> Report { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var instances = Instances.Get(context);
+
+            var results = new List<TopicModel>();
+
+            foreach (var item in instances)
+            {
+                results.Add(item.StopStreaming());
+            }
+
+            var report = new ReportModel()
+            {
+                Topics = results.Select(t => t.Topic).ToList(),
+                Authors = results.Sum(t => t.Authors.Count),
+                Tweets = results.Sum(t=>t.Tweets)
+            };
+
+            report.Terms = results.SelectMany(t => t.Terms)
+                                    .GroupBy(t => t.Term)
+                                    .OrderBy(t => t.Sum(t1 => t1.Count))
+                                    .Take(10)
+                                    .Select(t => t.Key)
+                                    .ToList();
+
+            Report.Set(context, report);
+        }
+    }
+}
