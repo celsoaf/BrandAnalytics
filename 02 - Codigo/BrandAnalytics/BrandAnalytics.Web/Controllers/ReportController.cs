9c503f45c6a91b3f011bd41678caeced8dec21d0
celsoaf <celsoaf@gmail.com>
2013-11-29 02:22:57 +0000

    Add reportController

---
+++ 02 - Codigo/BrandAnalytics/BrandAnalytics.Web/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BrandAnalytics.Data.Models;
+
+namespace BrandAnalytics.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        private BrandAnalyticsDB context = new BrandAnalyticsDB();
+
+        //
+        // GET: /Report/
+
+        public ActionResult Index(int id)
+        {
+            var model = context.Studies.FirstOrDefault(s => s.Id == id);
+
+            View(model);
+
+            return View();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
